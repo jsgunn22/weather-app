b15d1694db1299ba2ac37635bebb9ce789447dee
Jeffrey Gunn <j.s.gunn22@gmail.com>
2023-07-15 09:41:19 -0700

    add search bar and label behavior

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ $(function () {
   let body = $("body");
   let root = body.children().eq(1);
 
+  // Search Section
   let searchDiv = $("<div>");
   let searchBarDiv = $("<div>");
   let searchBarLabel = $("<label>");
@@ -14,7 +15,19 @@ $(function () {
   searchDiv.addClass("searchDiv");
   searchDiv.append(searchBarDiv);
   searchBarDiv.append(searchBarLabel);
-  searchBarDiv.addClass('searchBarDiv')
+  searchBarDiv.addClass("searchBarDiv");
   searchBarLabel.text("Search Cities");
   searchBarDiv.append(searchBar);
+  searchBar.addClass("medium-text");
+
+  // search bar label behavior
+  searchBar.on("focus", function () {
+    searchBarLabel.addClass("labelAct");
+    searchBar.on("focusout", function () {
+      if (!searchBar.val()) {
+        o("test");
+        searchBarLabel.removeClass("labelAct");
+      }
+    });
+  });
 });
