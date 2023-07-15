f81b21ed6863a447ae20b6aaa4ff860054c7d093
Jeffrey Gunn <j.s.gunn22@gmail.com>
2023-07-14 17:56:15 -0700

    adds initial JS for search area

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,20 @@
-console.log("test");
-
 function o(nion) {
   console.log(nion);
 }
 
 $(function () {
-  o("test");
+  let body = $("body");
+  let root = body.children().eq(1);
+
+  let searchDiv = $("<div>");
+  let searchBarDiv = $("<div>");
+  let searchBarLabel = $("<label>");
+  let searchBar = $("<input>");
+  root.append(searchDiv);
+  searchDiv.addClass("searchDiv");
+  searchDiv.append(searchBarDiv);
+  searchBarDiv.append(searchBarLabel);
+  searchBarDiv.addClass('searchBarDiv')
+  searchBarLabel.text("Search Cities");
+  searchBarDiv.append(searchBar);
 });
